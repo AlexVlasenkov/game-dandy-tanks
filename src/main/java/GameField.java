4c3fd12991ee7ab91152bd589cd9c08ae1d67550
a.vlasenkov <pointgred63@gmail.com>
2022-07-16 18:16:18 +0300

    add objects on map

--- src/main/java/GameField.java
+++ src/main/java/GameField.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public class GameField extends JPanel {
     // Размеры окна, включая заголовок
     private static final int WINDOW_WIDTH = 576;
-    private static final int WINDOW_HEIGHT = 576;
+    private static final int WINDOW_HEIGHT = 606;
     // Направление перемещения
     private final int UP = 1;
     private final int DOWN = 2;
@@ -16,11 +16,28 @@ public class GameField extends JPanel {
     private int tankX = 256;
     private int tankY = 256;
     // Размеры танка
-    private final int TANK_WIDTH = 64;
-    private final int TANK_HEIGHT = 64;
+    private final int OBJECT_WIDTH = 64;
+    private final int OBJECT_HEIGHT = 64;
     // Координаты пули
-    private int bulletX = 320;
-    private int bulletY = 320;
+    private int bulletX = -100;
+    private int bulletY = -100;
+    // Объекты карты
+    private final String BRICK = "BRICK";
+    private final String EARTH = "EARTH";
+    private final String WATER = "WATER";
+    private final String GREEN = "GREEN";
+    // Массив объектов карты; поле карты представляет собой матрицу 56x9
+    private String[][] objects = {
+            {EARTH, BRICK, BRICK, EARTH, BRICK, EARTH, GREEN, BRICK, BRICK},
+            {BRICK, EARTH, EARTH, EARTH, EARTH, EARTH, EARTH, EARTH, WATER},
+            {BRICK, EARTH, EARTH, BRICK, EARTH, EARTH, BRICK, EARTH, WATER},
+            {EARTH, EARTH, EARTH, EARTH, EARTH, EARTH, EARTH, BRICK, BRICK},
+            {BRICK, BRICK, EARTH, GREEN, EARTH, EARTH, EARTH, EARTH, BRICK},
+            {WATER, EARTH, EARTH, EARTH, EARTH, EARTH, BRICK, EARTH, BRICK},
+            {BRICK, EARTH, EARTH, BRICK, EARTH, EARTH, BRICK, EARTH, GREEN},
+            {EARTH, EARTH, EARTH, EARTH, EARTH, EARTH, EARTH, EARTH, BRICK},
+            {BRICK, GREEN, BRICK, BRICK, EARTH, BRICK, WATER, BRICK, WATER}
+    };
 
     // Движение танка
     private void move(int direction) throws Exception {
@@ -75,10 +92,10 @@ public class GameField extends JPanel {
 
     // Запуск игры
     private void runTheGame() throws Exception {
-        /*while (tankY != WINDOW_HEIGHT - TANK_HEIGHT - 44) {
-            move(DOWN);
-        }*/
-        fire();
+
+        while (true) {
+            fire();
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -102,10 +119,31 @@ public class GameField extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // Танк
-        g.setColor(Color.RED);
-        g.fillRect(tankX, tankY, TANK_WIDTH, TANK_HEIGHT);
-        // Пушка
+        // Рисуем объекты карты
+        for (int x = 0; x < objects.length; x++) {
+            for (int y = 0; y < objects.length; y++) {
+                switch (objects[y][x]) {
+                    case BRICK:
+                        g.setColor(new Color(246, 84, 20));
+                        break;
+                    case EARTH:
+                        g.setColor(new Color(244, 233, 246));
+                        break;
+                    case WATER:
+                        g.setColor(new Color(021, 172, 246));
+                        break;
+                    case GREEN:
+                        g.setColor(new Color(17, 82, 21, 255));
+                        break;
+                }
+                g.fillRect(x * OBJECT_WIDTH, y * OBJECT_WIDTH, OBJECT_WIDTH, OBJECT_HEIGHT);
+            }
+        }
+
+        // Рисуем танк
+        g.setColor(Color.DARK_GRAY);
+        g.fillRect(tankX, tankY, OBJECT_WIDTH, OBJECT_HEIGHT);
+        // Рисуем пушку
         g.setColor(Color.GREEN);
         switch (direction) {
             case UP:
@@ -122,6 +160,7 @@ public class GameField extends JPanel {
                 break;
         }
 
+        // Рисуем пулю
         g.setColor(Color.ORANGE);
         g.fillRect(bulletX, bulletY, 14, 14);
     }
