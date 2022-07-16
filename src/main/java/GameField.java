9f2353ce76f137bd11116dea8a164f28b5dba990
a.vlasenkov <pointgred63@gmail.com>
2022-07-16 16:54:23 +0300

    added bullet trace

--- src/main/java/GameField.java
+++ src/main/java/GameField.java
@@ -5,20 +5,20 @@ public class GameField extends JPanel {
     // Размеры окна, включая заголовок
     private static final int WINDOW_WIDTH = 576;
     private static final int WINDOW_HEIGHT = 576;
-    // Направление: 1 - Up, 2 - Down, 3 - Left, 4 - Right
-    private int direction;
-    // Положение окна
-    private int x = 256;
-    private int y = 256;
-    // Размеры танка
-    private final int TANK_WIDTH = 64;
-    private final int TANK_HEIGHT = 64;
     // Направление перемещения
     private final int UP = 1;
     private final int DOWN = 2;
     private final int LEFT = 3;
     private final int RIGHT = 4;
-    // Пуля
+    // Направление: 1 - Up, 2 - Down, 3 - Left, 4 - Right
+    private int direction = UP;
+    // Положение окна
+    private int tankX = 256;
+    private int tankY = 256;
+    // Размеры танка
+    private final int TANK_WIDTH = 64;
+    private final int TANK_HEIGHT = 64;
+    // Координаты пули
     private int bulletX = 320;
     private int bulletY = 320;
 
@@ -28,16 +28,16 @@ public class GameField extends JPanel {
 
         switch (direction) {
             case UP:
-                y--;
+                tankY--;
                 break;
             case DOWN:
-                y++;
+                tankY++;
                 break;
             case LEFT:
-                x--;
+                tankX--;
                 break;
             case RIGHT:
-                x++;
+                tankX++;
                 break;
         }
 
@@ -45,11 +45,40 @@ public class GameField extends JPanel {
         repaint();
     }
 
+    // Выстрел из танка
+    private void fire() throws Exception {
+        bulletX = tankX + 25;
+        bulletY = tankY + 25;
+
+        while (bulletX > 0 && bulletX < WINDOW_WIDTH && bulletY > 0 && bulletY < WINDOW_HEIGHT) {
+            switch (direction) {
+                case 1:
+                    bulletY--;
+                    break;
+                case 2:
+                    bulletY++;
+                    break;
+                case 3:
+                    bulletX--;
+                    break;
+                case 4:
+                    bulletX++;
+                    break;
+            }
+            Thread.sleep(10);
+            repaint();
+        }
+
+        // По мере удаления пуля исчезает
+        bulletX = -100;
+    }
+
     // Запуск игры
     private void runTheGame() throws Exception {
-        while (y != WINDOW_HEIGHT - TANK_HEIGHT - 44) {
+        /*while (tankY != WINDOW_HEIGHT - TANK_HEIGHT - 44) {
             move(DOWN);
-        }
+        }*/
+        fire();
     }
 
     public static void main(String[] args) throws Exception {
@@ -75,21 +104,21 @@ public class GameField extends JPanel {
         super.paintComponent(g);
         // Танк
         g.setColor(Color.RED);
-        g.fillRect(x, y, TANK_WIDTH, TANK_HEIGHT);
+        g.fillRect(tankX, tankY, TANK_WIDTH, TANK_HEIGHT);
         // Пушка
         g.setColor(Color.GREEN);
         switch (direction) {
             case UP:
-                g.fillRect(x + 20, y, 24, 34);
+                g.fillRect(tankX + 20, tankY, 24, 34);
                 break;
             case DOWN:
-                g.fillRect(x + 20, y + 30, 24, 34);
+                g.fillRect(tankX + 20, tankY + 30, 24, 34);
                 break;
             case LEFT:
-                g.fillRect(x, y + 20, 34, 24);
+                g.fillRect(tankX, tankY + 20, 34, 24);
                 break;
             case RIGHT:
-                g.fillRect(x + 30, y + 20, 34, 24);
+                g.fillRect(tankX + 30, tankY + 20, 34, 24);
                 break;
         }
 
